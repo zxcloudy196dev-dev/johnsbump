70a7d5a99e3a16973078b0f12fb588fe093d38b6
zxcloudy196dev-dev <zxcloudy196dev@gmail.com>
2026-03-14 13:05:26 +0300

    Add files via upload

---
+++ WIP/config.py
@@ -0,0 +1,8 @@
+# config.py
+CONFIG = {
+    "API_TOKEN": "478664:AA183lxiXRK06NSmAwdRvT19VjY40ewX5RA",
+    "DEFAULT_ASSET": "USDT",
+    "DEFAULT_EXPIRY": 3600,  # 1 час в секундах
+    "ALLOW_ANONYMOUS": False,
+    "ALLOW_COMMENTS": False
+}
