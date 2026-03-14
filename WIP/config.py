6d09bf23b7757f8841509b5b86b20c409a35a33d
zxcloudy196dev-dev <zxcloudy196dev@gmail.com>
2026-03-14 13:39:18 +0300

    Delete WIP directory

--- WIP/config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-CONFIG = {
-    "API_TOKEN": "478664:AA183lxiXRK06NSmAwdRvT19VjY40ewX5RA",
-    "DEFAULT_ASSET": "USDT",
-    "DEFAULT_EXPIRY": 3600,  # 1 час в секундах
-    "ALLOW_ANONYMOUS": False,
-    "ALLOW_COMMENTS": False
-}
